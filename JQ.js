da882e0a787f8a95a8dcd6062acddabe12f34353
nocturnalforever <157411977+nocturnalforever@users.noreply.github.com>
2024-09-29 23:16:53 +0200

    Update JQ.js

--- JQ.js
+++ JQ.js
@@ -137,7 +137,7 @@ $('document').ready(function () {
     })
 
     // Set the date we're counting down to
-    var countDownDate = new Date("Nov 1, 2024 18:00:00").getTime();
+    var countDownDate = new Date("Nov 1, 2024 20:00:00").getTime();
 
     // Update the count down every 1 second
     var x = setInterval(function () {
@@ -161,9 +161,7 @@ $('document').ready(function () {
         // If the count down is finished, write some text
         if (distance < 0) {
             clearInterval(x);
-            document.getElementById("demo").innerHTML = "WE'RE RAVING!! COME JOIN US!";
+            document.getElementById("demo").innerHTML = "THE PIT IS OPEN!! COME JOIN US!";
         }
     }, 1000);
 })
-
-
