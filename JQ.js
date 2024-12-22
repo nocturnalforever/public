09af4b1f2690ebbb2652c78fef05fc1e907a3512
nocturnalforever <157411977+nocturnalforever@users.noreply.github.com>
2024-12-22 20:56:52 +0200

    Update JQ.js

--- JQ.js
+++ JQ.js
@@ -138,7 +138,7 @@ $('document').ready(function () {
     })
 
     // Set the date we're counting down to
-    var countDownDate = new Date("Nov 1, 2024 20:00:00").getTime();
+    var countDownDate = new Date("Dec 31, 2024 20:00:00").getTime();
 
     // Update the count down every 1 second
     var x = setInterval(function () {
